9dd1be7604a67e51aaca0f033cfa350ff7b9a61d
Tejanin <theexama123@gmail.com>
2024-03-06 12:11:59 -0400

    Creating the project

---
+++ BenchmarkAnts/Program.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarkAnts
+{
+    
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+        }
+    }
+}
